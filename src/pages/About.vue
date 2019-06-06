94df85c2e4347e6466dbabe344cc60bf4ecec447
Tyler Fisher <tyler@tylerjfisher.com>
2019-06-06 12:09:39 -0400

    start an about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,21 @@
 <template>
-  <Layout :showLinks="true">
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+  <Layout>
+    <article>
+      <h2>Our mission</h2>
+      <p>News Catalyst provides tools, fosters collaboration, and promotes experimentation in service of building digitally sustainable news organizations.</p>
+
+      <h2>Our staff</h2>
+      <div class="staffer">
+        <h5>Aron Pilhofer, Director</h5>
+        <g-image class="headshot" src="~/assets/aron.jpeg" />
+        <p>Aron Pilhofer is the James B. Steele Chair in Journalism Innovation at Temple University. In addition to teaching, his work is focused on new business models, digital transformation and innovation in news. Before joining Temple, Pilhofer was executive editor, digital, and interim chief digital officer at the Guardian in London. There, he led the Guardian's product and technology teams as well as heading visual journalism - including pictures, graphics, interactive and data journalism. Before coming to the Guardian, Aron was associate managing editor for digital strategy and editor of interactive news at The New York Times. Outside the newsroom, Aron co-founded two news-related startups: DocumentCloud and Hacks/Hackers.</p>
+      </div>
+      <div class="staffer">
+        <h5>Tyler Fisher, Deputy Director</h5>
+        <g-image class="headshot" src="~/assets/tyler.jpg" />
+        <p>Tyler is a journalist and developer based in Washington, D.C. As Deputy Director, Tyler is responsible for the tools and technology News Catalyst provides to its audience. Before joining News Catalyst, Tyler was a Senior News Apps Developer on POLITICO's Interactive News Team. There, he led development of POLITICO's 2018 election results coverage and built vital team infrastructure for rapidly publishing data-driven, bespoke web presentations. Before POLITICO, Tyler worked as a News Apps Developer on the NPR Visuals Team.</p>
+      </div>
+    </article>
   </Layout>
 </template>
 
@@ -12,3 +26,28 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+article {
+  max-width: 45rem;
+  padding: 3rem 2rem;
+  margin: auto;
+  font-size: 1.1rem;
+  line-height: 1.5;
+
+  @media screen and (min-width: 768px) {
+    font-size: 1.25rem;
+    line-height: 1.65;
+  }
+}
+
+.headshot {
+  width: 150px;
+  height: auto;
+  float: left;
+  border: 1px solid grey;
+  margin: 0 15px 10px 0;
+  border-radius: 75px;
+}
+</style>
+
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,9 +7,7 @@
 
           <p>At News Catalyst, we believe the only way forward is together. To that end, we’re working to connect news organizations who face common technological struggles and work with them to solve problems collectively.</p>
 
-          <p>First, who are we? We’re News Catalyst, a new project based out of Temple University’s Klein College of Media and Communication and part of the Knight-Lenfest Local News Transformation Fund.</p>
-
-          <p>Right now, there’s just two of us: Aron Pilhofer, our director and the James B. Steele Chair in Journalism Innovation at Temple University; and Tyler Fisher, our deputy director and formerly of POLITICO and NPR. We both have experience in pairing technical innovation with tangible impact on the business of news. More importantly, <a href="#jobs">we’re hiring!</a></p>
+          <p>First, who are we? We’re News Catalyst, a new project based out of Temple University’s Klein College of Media and Communication and part of the Knight-Lenfest Local News Transformation Fund. Read more about our staff and history <g-link to="/about">here</g-link>.</p>
         </div>
       </div>
       <div class="reverse">
@@ -42,8 +40,8 @@
       </div>
       <div class="action">
         <div class="block">
-          <h2 class="lede">For us to get started, we need your help.</h2>
-          <p><strong>Apply for our open positions</strong></p>
+          <h2 class="lede">We need your help</h2>
+          <h3>Apply for our open positions</h3>
           <p>We&rsquo;re hiring! Take a look at our open positions:</p>
           <div id="jobs" v-for="edge in $page.jobs.edges" :key="edge.node.id">
             <ul>
@@ -51,10 +49,10 @@
             </ul>
           </div>
 
-          <p><a href="https://newscatalyst.org/news-tools-census"><strong>Take our Tools Census</strong></a></p>
+          <h3><a href="https://newscatalyst.org/news-tools-census">Take our Tools Census</a></h3>
           <p>To start building the tool suite that we will offer through PressPass, we&rsquo;re starting with some research. You can help with that research by taking our <a href="https://newscatalyst.org/news-tools-census">Tools Census</a>. We want to know about the tools you use day-to-day in your job as a journalist.</p>
 
-          <p><strong>Get in touch</strong></p>
+          <h3>Get in touch</h3>
           <p>Have something to pitch us? Want to tell us how we got it all wrong? Email us at hi@newscatalyst.org.</p>
         </div>
       </div>
@@ -77,21 +75,31 @@ query Jobs {
 </page-query>
 
 <script>
+export default {
+  metaInfo: {
+    title: 'News Catalyst'
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-  @import '../styles/angled-edges.scss';
-
   p, ul, ol, h1, h2, h3, h4, h5, h6 {
     color: #111;
   }
 
-.lede {
-  font-size: 1.33rem;
-  @media screen and (min-width: 768px) {
-    font-size: 1.75rem;
+  .lede {
+    font-size: 1.33rem;
+    @media screen and (min-width: 768px) {
+      font-size: 1.75rem;
+    }
   }
-}
+
+  h2.lede {
+    font-size: 2rem;
+    @media screen and (min-width: 768px) {
+      font-size: 2.5rem;
+    }
+  }
 
   p, ul, ol {
     font-size: 1.1rem;
