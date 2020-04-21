3d49f39c03732e6c33b4d31430c70b633557fd2b
Tyler Fisher <tyler@tylerjfisher.com>
2020-04-21 10:16:11 -0400

    Product partnerships page

---
+++ src/pages/ProductPartnerships.vue
@@ -0,0 +1,127 @@
+<template>
+  <Layout>
+    <article>
+      <h2 id="Product Challenge Partnerships">Product Challenge Partnerships</h2>
+      <p>News Catalyst is a two-year, grant-funded project that provides tools, fosters collaboration, and promotes experimentation in service of building digitally sustainable news organizations. The project is based at Temple University’s Klein College of Media and Communication. </p>
+
+      <p>We’re seeking to collaborate with partner news organizations on a product-focused challenge in your news organization. </p>
+
+      <p>Partnering with News Catalyst on a product problem means that we assist with identifying and solving a specific product roadblock or challenge within your organization.</p>
+
+      <p>This could take the form of things like:</p>
+      <ul><li>helping your teams develop a shared understanding of product development
+      </li><li>developing a better process around product development
+      </li><li>organizing your teams around a specific product you seek to develop
+      </li></ul>
+
+      <p>We’re pretty broad in our definition of what could fit this challenge and want to hear what you need help with.</p>
+
+      <p>We’re looking for partners who embrace transparency in this collaboration. We want to be able to share insights and takeaways, what works and what’s difficult. It’s our goal to share replicable solutions to the larger field of journalism.</p>
+
+      <p>Our partnership comes at no cost to your organization. We may be able to make some resources available for costs that come up during the process and ask that our partners do the same.</p>
+
+      <h3 id="Process">Process</h3>
+
+      <p><b>Tell us about your challenge</b></p>
+      <p>If you’re interested in working with us to address a product challenge in your news organization, tell us more about what challenge you’re trying to solve. We’re looking to team up with news organizations on a rolling basis and will engage with projects as we have the bandwidth.</p>
+
+      <p><b>Conversation</b></p>
+      <p>If there is a potential fit, we will schedule an informational call with you to learn more about your challenge. Following that call, we will evaluate whether the challenge fits with our mission and if the scope works for our current bandwidth. If the project is a good fit but there is a mismatch in capacity, we may ask if it’s possible to table the partnership until a later date.</p>
+
+      <p><b>Partnership</b></p>
+      <p>If the project is a good fit for us and for you, we will schedule further conversations to establish the full scope of the challenge, outline the terms of the partnership, mutually decide on goals and deliverables and establish an MOU that guides the work.</p>
+
+      <h3 id="FAQ">FAQ</h3>
+
+      <p><b>Who are we and how can we help?</b></p>
+      <p>News Catalyst is a grant-funded project building the resources and capacity in news organizations to do focused product work with the goal of supporting newsroom sustainability. Our expertise spans editorial processes, collaboration, product management and technology.</p>
+
+      <p>Our team is made up of:</p>
+      <ul><li>Aron Pilhofer, Director
+      </li><li>Tyler Fisher, Deputy Director, Technology
+      </li><li>Jacqui Lough, Developer
+      </li><li>Heather Bryant, Deputy Director, Product
+      </li><li>Jessica Morrison, Product Lead
+      </li></ul>
+
+      <p>You can read more about us here:  <a href="https://newscatalyst.org/about">About News Catalyst</a> </p>
+
+      <p><b>Who would you be working with?</b></p>
+      <p>Various members of the News Catalyst team may be involved at different points in the process as their expertise is relevant.</p>
+
+      <p><b>What do you mean by product challenge?</b></p>
+      <p>We define this pretty broadly because challenges manifest in numerous ways across newsroom contexts. Perhaps it’s the complete absence of a product development culture or process, maybe you’ve been doing product development work all along but you’ve never called it that and could be helped by connecting with more of the structures and workflow of a product development process. You are interested in an idea and need help organizing your team around it. You could be a completely new organization and want to build a product-driven culture from the get-go or a long-running newsroom that’s trying to adapt more nimbly. Talk to us about what you think is a challenge for you.</p>
+
+      <p><b>What does it cost?</b></p>
+      <p>The contributions of the team members from News Catalyst do not cost anything. We are a grant-funded project that is designed specifically for these kinds of partnerships. </p>
+
+      <p>We do ask that you have some level investment in this process, whether it’s committing to have people who are tasked to work on the partnership or contributing to costs that may arise during our partnership. For example, if we mutually agree that an additional consultant or technology component is needed. We’ll work with you to figure out how to cover such things and may be able to contribute as well.</p>
+
+      <p><b>How long are the partnerships?</b></p>
+      <p>It varies by the partnership. Most will likely fall in the few weeks to a few months range.</p>
+
+      <p><b>What types of things will be discussed or shared publicly?</b></p>
+      <p>The goal of transparency is to make available all the things we learn from addressing product challenges in a wide variety of newsrooms. This may be design concepts, metrics, open source technologies and approaches for organizing people and teams.</p>
+
+      <p><b>Questions for us?</b></p>
+      <p>You are welcome to reach out with questions you have about this process. Email <a href="mailto:heather@newscatalyst.org">heather@newscatalyst.org</a> </p>
+
+      <p><b>Application (Airtable Form to Embed)</b></p>
+
+      <iframe class="airtable-embed" src="https://airtable.com/embed/shrBTenn8Nqtv0Cvb?backgroundColor=purple" frameborder="0" onmousewheel="" width="100%" height="800" style="background: transparent; border: 1px solid #ccc;"></iframe>
+
+    </article>
+  </Layout>
+</template>
+
+<script>
+const desc = "News Catalyst helps news organizations transform themselves into sustainable digital businesses.";
+
+export default {
+  metaInfo: {
+    title: 'About',
+    meta: [
+      {
+        name: "description",
+        content: desc,
+      },
+      {
+        name: "og:title",
+        content: "News Catalyst",
+      },
+      {
+        name: "og:description",
+        content: desc,
+      },
+      {
+        name: "og:url",
+        content: "https://newscatalyst.org/about",
+      },
+      {
+        name: "twitter:title",
+        content: "News Catalyst",
+      },
+      {
+        name: "twitter:description",
+        content: desc,
+      },
+    ],
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+article {
+  max-width: 45rem;
+  padding: 3rem 2rem;
+  margin: auto;
+  font-size: 1.1rem;
+  line-height: 1.5;
+
+  @media screen and (min-width: 768px) {
+    font-size: 1.25rem;
+    line-height: 1.65;
+  }
+}
+</style>
+
